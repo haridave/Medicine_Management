00f1648ad590f66191209f739252e9b100786bfb
Peter <petrusfourie@hotmail.com>
2016-06-26 15:56:09 +1200

    Added project to GitHub

---
+++ MedicineMaster/MedicineMaster/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicineMaster.Startup))]
+namespace MedicineMaster
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
